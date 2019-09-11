5334edda66820967fe86b3343a501ed33a19f113
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2019-09-11 04:44:19 +0300

    Add context processors tests

---
+++ tests/test_context_processors.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# django-project-version
+# tests/test_context_processors.py
+
+
+from typing import Dict, List  # pylint: disable=W0611
+
+from django.http import HttpRequest
+from django.test import TestCase
+from django.utils import timezone
+
+from djversion.context_processors import version
+
+
+__all__ = ["VersionContextProcessorTest"]  # type: List[str]
+
+
+YEAR = timezone.now().today().year  # type: int
+
+
+class VersionContextProcessorTest(TestCase):
+    """
+    Version context processor tests.
+    """
+
+    def test_version(self) -> None:
+        """
+        Must return formatted new context variable with version.
+
+        :return: nothing.
+        :rtype: None.
+        """
+
+        request = HttpRequest()  # type: HttpRequest
+        result = version(request=request)  # type: Dict[str, str]
+
+        self.assertIsInstance(obj=result, cls=dict)
+        self.assertDictEqual(d1=result, d2={"VERSION": "1.0.1 (Aug. 24, 1991)"})
